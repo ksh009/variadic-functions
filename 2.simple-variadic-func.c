59c65467e1a276ca9b9412e95faebf583678be69
HughesSeanKyle <khughessean@yahoo.com>
2023-05-07 19:04:00 +0200

    Uddate notes

--- 2.simple-variadic-func.c
+++ 2.simple-variadic-func.c
@@ -8,6 +8,9 @@ int sum(int count, ...)
     int sum = 0;
     for (int i = 0; i < count; i++)
     {
+        /*
+            `va_arg()` retrieves the next argument from the `va_list` and advances the POINTER to the next argument. So, every time the function is called then the pointer moves to the next arg. The loop does not effect the list iteration directly, but merely calls the function (va_args) to create an iteration for the length of the count - 1.
+        */
         int arg = va_arg(args, int);
         sum += arg;
         printf("%d ", arg);
